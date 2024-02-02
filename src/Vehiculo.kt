53581cbabd16af10def833294e679e023c3ad396
mrodazo617 <mrodazo617@g.educaand.es>
2024-02-02 20:58:48 +0100

    Tema 5 Ejercicio 1 vehículos prácticamente listo

--- src/Vehiculo.kt
+++ src/Vehiculo.kt
@@ -1,10 +1,33 @@
-class Vehiculo (val marca: String, val modelo: String, var capCombustible: Int){
+open class Vehiculo (val marca: String, val modelo: String, val capCombustible: Int){
 
-    fun mostrarInformacion() {
-        println("Vehículo marca $marca, modelo $modelo y capacidad de combustible $capCombustible litros")
+    override fun toString(): String {
+        return "Vehículo marca $marca, modelo $modelo y capacidad de combustible $capCombustible litros"
     }
 
-    fun calcularAutonomia(capCombustible: Int) : Int {
+    open fun calcularAutonomia() : Int {
+        val autonomia = capCombustible*10
+        return autonomia
+    }
+}
 
+class Automovil(marca: String, modelo: String, capCombustible: Int, val tipo: String) :Vehiculo(marca,modelo,capCombustible){
+
+    override fun toString(): String {
+        return "Automóvil marca $marca, modelo $modelo, de tipo $tipo, con capacidad de combustible $capCombustible litros"
+    }
+
+    override fun calcularAutonomia(): Int {
+        return (super.calcularAutonomia()+100)
+    }
+}
+
+class Motocicleta (marca: String, modelo: String, capCombustible: Int, val cilindrada: Int): Vehiculo(marca, modelo,capCombustible){
+
+    override fun toString(): String {
+        return "Motocileta marca $marca, modelo $modelo, de cilindrada $cilindrada, con capacidad de combustible $capCombustible litros"
+    }
+
+    override fun calcularAutonomia(): Int {
+        return (super.calcularAutonomia()-40)
     }
 }
